ef3c675523ef265cb7dc1477c941e666919d1fa7
rishab-nahar <rishabnahar2025@gmail.com>
2022-05-18 18:20:16 +0530

    removed deadCode from getImage

--- functions/getImage.js
+++ functions/getImage.js
@@ -2,13 +2,15 @@ const imageModel = require(__dirname + "/../models/imageModel.js")
 
 const getImageById = async (id) => {
 	const img = await imageModel.findById(id).exec();
-	if(!img) {
+	if (!img) {
 		return null;
 	}
 	const ret = {
-		contentType : img.contentType,
-		data : img.data.toString('base64')
+		contentType: img.contentType,
+		data: img.data.toString('base64')
 	}
 	return ret;
 }
-module.exports = getImageById;
+
+
+

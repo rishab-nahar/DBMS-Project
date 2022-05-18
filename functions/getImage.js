c70b2cd5e1d7c6f1aacc18790e9df0101ec342ff
rishab-nahar <rishabnahar2025@gmail.com>
2022-05-18 19:19:13 +0530

    trying to get conflict

--- functions/getImage.js
+++ functions/getImage.js
@@ -1,15 +1,11 @@
 const imageModel = require(__dirname + "/../models/imageModel.js")
 
-const getImageById = async (id) => {
-	const img = await imageModel.findById(id).exec();
-	if (!img) {
-		return null;
-	}
-	const ret = {
-		contentType: img.contentType,
-		data: img.data.toString('base64')
-	}
-	return ret;
+
+const ret = {
+	contentType: img.contentType,
+	data: img.data.toString('base64')
+}
+return ret;
 }
 
 abcdefghi

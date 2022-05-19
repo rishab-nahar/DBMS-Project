fde9e803fcba46acf1aa5d3b000c7f60a39e2b7b
rishab-nahar <rishabnahar2025@gmail.com>
2022-05-19 12:46:34 +0530

    asdasdasd asdas das dsd

--- functions/getImage.js
+++ functions/getImage.js
@@ -1,6 +1,8 @@
 const imageModel = require(__dirname + "/../models/imageModel.js")
 
-
+const extra = async () => {
+	console.log("hello world");
+}
 
 const getImageById = async (id) => {
 	const img = await imageModel.findById(id).exec();
